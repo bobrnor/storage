618d7f12cdd1035db371eca010f1fab4e8adddfb
Daniil Zinenko <daniil.zinenko@iqoption.com>
2018-05-05 15:35:27 +0300

    boltdb support

---
+++ boltdb.go
@@ -0,0 +1,242 @@
+package storage
+
+import (
+	"log"
+	"time"
+
+	"strconv"
+
+	"encoding/json"
+
+	"encoding/binary"
+
+	"strings"
+
+	"github.com/boltdb/bolt"
+	"github.com/mailhog/data"
+)
+
+// BoltDB represents BoltDB backed storage backend
+type BoltDB struct {
+	db     *bolt.DB
+	bucket []byte
+}
+
+// CreateBoltDB creates a BoltDB backed storage backend
+func CreateBoltDB(path, bucket string) *BoltDB {
+	db, err := bolt.Open(path, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Printf("Error opening BoltDB database: %s", err)
+		return nil
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Error creating BoltDB bucket: %s", err)
+		return nil
+	}
+
+	return &BoltDB{
+		db:     db,
+		bucket: []byte(bucket),
+	}
+}
+
+// Store stores a message in BoltDB and returns its storage ID
+func (b *BoltDB) Store(m *data.Message) (string, error) {
+	err := b.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(b.bucket)
+
+		id, err := bucket.NextSequence()
+		if err != nil {
+			return err
+		}
+
+		m.ID = data.MessageID(strconv.FormatUint(id, 10))
+		buf, err := json.Marshal(m)
+		if err != nil {
+			return err
+		}
+
+		return bucket.Put(itob(id), buf)
+	})
+
+	return string(m.ID), err
+}
+
+func itob(v uint64) []byte {
+	b := make([]byte, 16)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+// Count returns the number of stored messages
+func (b *BoltDB) Count() int {
+	var count int
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(b.bucket)
+		stats := bucket.Stats()
+		count = stats.KeyN
+		return nil
+	})
+
+	if err != nil {
+		return 0
+	}
+
+	return count
+}
+
+// Search finds messages matching the query
+func (b *BoltDB) Search(kind, query string, start, limit int) (*data.Messages, int, error) {
+	var matchFunc func(data.Message) bool
+	switch kind {
+	case "to":
+		matchFunc = func(m data.Message) bool {
+			for _, to := range m.Raw.To {
+				return strings.Contains(to, query)
+			}
+			return false
+		}
+	case "from":
+		matchFunc = func(m data.Message) bool {
+			return strings.Contains(m.Raw.From, query)
+		}
+	default:
+		matchFunc = func(m data.Message) bool {
+			return strings.Contains(m.Raw.Data, query)
+		}
+	}
+
+	count := 0
+	messages := data.Messages{}
+
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(b.bucket)
+
+		c := bucket.Cursor()
+
+		var i int
+		var m data.Message
+
+		for k, v := c.Last(); k != nil; k, v = c.Prev() {
+			if err := json.Unmarshal(v, &m); err != nil {
+				return err
+			}
+
+			if matchFunc(m) {
+				count++
+
+				if i >= start && i < start+limit {
+					messages = append(messages, m)
+
+					if len(messages) >= limit {
+						return nil
+					}
+				}
+			}
+
+			i++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Error filtering messages: %s", err)
+		return nil, 0, err
+	}
+
+	return &messages, count, nil
+}
+
+// List returns a list of messages by index
+func (b *BoltDB) List(start int, limit int) (*data.Messages, error) {
+	messages := data.Messages{}
+
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(b.bucket)
+
+		c := bucket.Cursor()
+
+		var i int
+		var m data.Message
+
+		for k, v := c.Last(); k != nil; k, v = c.Prev() {
+			if i < start {
+				i++
+				continue
+			}
+
+			if err := json.Unmarshal(v, &m); err != nil {
+				return err
+			}
+
+			messages = append(messages, m)
+
+			if len(messages) >= limit {
+				return nil
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Error listing messages: %s", err)
+		return nil, err
+	}
+
+	return &messages, nil
+}
+
+// DeleteOne deletes an individual message by storage ID
+func (b *BoltDB) DeleteOne(id string) error {
+	idUint, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	return b.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(b.bucket)
+		return bucket.Delete(itob(idUint))
+	})
+}
+
+// DeleteAll deletes all messages stored in BoltDB
+func (b *BoltDB) DeleteAll() error {
+	return b.db.Update(func(tx *bolt.Tx) error {
+		if err := tx.DeleteBucket(b.bucket); err != nil {
+			return err
+		}
+
+		_, err := tx.CreateBucket(b.bucket)
+		return err
+	})
+}
+
+// Load loads an individual message by storage ID
+func (b *BoltDB) Load(id string) (*data.Message, error) {
+	idUint, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	result := data.Message{}
+
+	err = b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(b.bucket)
+		buf := bucket.Get(itob(idUint))
+
+		return json.Unmarshal(buf, &result)
+	})
+
+	return &result, err
+}
---
+++ boltdb_test.go
@@ -0,0 +1,299 @@
+package storage
+
+import (
+	"testing"
+
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/mailhog/data"
+)
+
+func TestCreateBoltDB(t *testing.T) {
+	type args struct {
+		path   string
+		bucket string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "db in tmp",
+			args: args{
+				path:   "/tmp/db",
+				bucket: "test",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CreateBoltDB(tt.args.path, tt.args.bucket); got == nil {
+				t.Errorf("CreateBoltDB() = %v", got)
+			} else {
+				got.db.Close()
+			}
+		})
+	}
+}
+
+func TestBoltDB_Store(t *testing.T) {
+	db := CreateBoltDB("/tmp/db", "test")
+	defer db.db.Close()
+
+	type fields struct {
+		db     *bolt.DB
+		bucket []byte
+	}
+	type args struct {
+		m *data.Message
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "good test",
+			fields: fields{
+				db:     db.db,
+				bucket: db.bucket,
+			},
+			args: args{
+				m: &data.Message{
+					ID: "",
+					From: &data.Path{
+						Relays:  nil,
+						Mailbox: "from@test.com",
+						Domain:  "",
+						Params:  "",
+					},
+					To: []*data.Path{
+						{
+							Relays:  nil,
+							Mailbox: "to@test.com",
+							Domain:  "",
+							Params:  "",
+						},
+					},
+					Content: &data.Content{
+						Headers: nil,
+						Body:    "test body",
+						Size:    9,
+						MIME:    &data.MIMEBody{},
+					},
+					Created: time.Now(),
+					MIME:    &data.MIMEBody{},
+					Raw: &data.SMTPMessage{
+						From: "from@test.com",
+						To:   []string{"to@test.com"},
+						Data: "test body",
+						Helo: "",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &BoltDB{
+				db:     tt.fields.db,
+				bucket: tt.fields.bucket,
+			}
+			got, err := b.Store(tt.args.m)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BoltDB.Store() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) == 0 {
+				t.Errorf("BoltDB.Store() = %v", got)
+			}
+		})
+	}
+}
+
+func TestBoltDB_Count(t *testing.T) {
+	b := CreateBoltDB("/tmp/db", "test")
+	defer b.db.Close()
+
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "good test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := b.Count(); got == 0 {
+				t.Errorf("BoltDB.Count() = %v", got)
+			}
+		})
+	}
+}
+
+func TestBoltDB_Search(t *testing.T) {
+	b := CreateBoltDB("/tmp/db", "test")
+	defer b.db.Close()
+
+	type args struct {
+		kind  string
+		query string
+		start int
+		limit int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "good test",
+			args: args{
+				kind:  "to",
+				query: "to@",
+				start: 0,
+				limit: 100,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := b.Search(tt.args.kind, tt.args.query, tt.args.start, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BoltDB.Search() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got == nil || len(*got) == 0 {
+				t.Errorf("BoltDB.Search() got = %v", got)
+			}
+			if got1 == 0 {
+				t.Errorf("BoltDB.Search() got1 = %v", got1)
+			}
+		})
+	}
+}
+
+func TestBoltDB_List(t *testing.T) {
+	b := CreateBoltDB("/tmp/db", "test")
+	defer b.db.Close()
+
+	type args struct {
+		start int
+		limit int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "good test",
+			args: args{
+				start: 0,
+				limit: 100,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := b.List(tt.args.start, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BoltDB.List() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got == nil || len(*got) == 0 {
+				t.Errorf("BoltDB.List() = %v", got)
+			}
+		})
+	}
+}
+
+func TestBoltDB_Load(t *testing.T) {
+	b := CreateBoltDB("/tmp/db", "test")
+	defer b.db.Close()
+
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "good test",
+			args: args{
+				id: "1",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := b.Load(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BoltDB.Load() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got == nil {
+				t.Errorf("BoltDB.Load() = %v", got)
+			}
+		})
+	}
+}
+
+func TestBoltDB_DeleteOne(t *testing.T) {
+	b := CreateBoltDB("/tmp/db", "test")
+	defer b.db.Close()
+
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "good test",
+			args: args{
+				id: "1",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := b.DeleteOne(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("BoltDB.DeleteOne() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestBoltDB_DeleteAll(t *testing.T) {
+	b := CreateBoltDB("/tmp/db", "test")
+	defer b.db.Close()
+
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "good test",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := b.DeleteAll(); (err != nil) != tt.wantErr {
+				t.Errorf("BoltDB.DeleteAll() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
